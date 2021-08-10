3b233d4d23d333e4ede83143efe45205a44f3d96
Do-code-ing <zxcvbn658@naver.com>
2021-08-11 04:44:02 +0900

    2021-08-11

--- Keywords/08_class.py
+++ Keywords/08_class.py
@@ -1,7 +1,7 @@
 # class
 # 새로운 사용자 정의 class를 정의하는 데 사용된다.
 # class는 실제 상황을 표현하려는 관련 attributes 및 methods의 모음이다.
-# data와 함수를 한 calss에 모으는 이 아이디어는 OOP(객체지향프로그래밍) 개념의 핵심이다.
+# data와 함수를 한 class에 모으는 이 아이디어는 OOP(객체지향프로그래밍) 개념의 핵심이다.
 
 class ExampleClass:
     def function1(parameters):
